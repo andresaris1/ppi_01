60b5d191fcb7e7df4f48f0b67bf8fc054793f891
andresaris1 <124937113+andresaris1@users.noreply.github.com>
2023-03-12 15:01:02 -0500

    Update Hola mundo.py

--- Hola mundo.py
+++ Hola mundo.py
@@ -2,5 +2,5 @@
 print('Hola mundo')
 
 #Imprimir Hola + "Nombre" de cada integrante del proyecto
-print('Andres_Felipe_Arismendi_Alzate')
+print('Hola, Andres Felipe Arismendi Alzate')
 print('Hola, Mariana Hernández')
