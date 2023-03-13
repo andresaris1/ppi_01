d18e36dfbd6459b49a2bbeb1861ba332ec38b392
fmunoze <fmunoze@unal.edu.co>
2023-03-12 19:48:01 -0500

    Update Hola mundo.py

--- Hola mundo.py
+++ Hola mundo.py
@@ -4,3 +4,4 @@ print('Hola mundo')
 #Imprimir Hola + "Nombre" de cada integrante del proyecto
 print('Hola, Andres Felipe Arismendi Alzate')
 print('Hola, Mariana Hernández')
+print('Hola, Felipe Muñoz Echeverri')
