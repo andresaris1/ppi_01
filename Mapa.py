6590a5ee48769a57ccf3ece81bf91f603718d081
marianahernandez <marhernandezpa@unal.edu.co>
2023-03-23 07:27:42 -0500

    Mapa del Valle de aburra generado en python y esqueleto index

---
+++ Mapa.py
@@ -0,0 +1,33 @@
+import folium
+import pandas as pd
+import geopandas as gpd
+import numpy as np
+
+
+municipios = gpd.read_file("/Users/marianahernandez/biciMaps/MunicipiosVeredas.json")
+municipios_index = municipios.copy()
+municipios_index = municipios_index.set_index("MPIO_CNMBR")
+# Eliminar columnas innecesarias
+municipios = municipios.drop(columns = ["MPIO_CCNCT", "MPIO_CCDGO"])
+# Municipios de antioquia
+municipios_ant = municipios[municipios["DPTO_CCDGO"] == "05"]
+municipios_valle = municipios_index[municipios_index["DPTO_CCDGO"] == "05"]
+municipios_valle = municipios_valle.loc[["MEDELLÍN", "ENVIGADO", "SABANETA","ITAGÜÍ", "CALDAS", "LA ESTRELLA", "COPACABANA", "BELLO", "BARBOSA", "GIRARDOTA"]]
+municipios_valle = municipios_valle.reset_index()
+print(municipios_valle.head())
+
+# Crear mapa locacion, estilo, zoom
+m = folium.Map(location=[6.25 , -75.6], tiles='cartodbpositron', zoom_start=10)
+
+for _, r in municipios_valle.iterrows():
+    # Simplificar la tolerancia para que el mapa se muestre
+    sim_geo = gpd.GeoSeries(r['geometry']).simplify(tolerance=0.001)
+    geo_j = sim_geo.to_json()
+    # Dar formato al poligono
+    geo_j = folium.GeoJson(data=geo_j,
+                           style_function=lambda x: {'fillColor': 'green'}) 
+    # Agregar pop-up con el nombre del municipio
+    folium.Popup(r['MPIO_CNMBR']).add_to(geo_j) 
+    # Agregar poligono al mapa
+    geo_j.add_to(m)
+m.save('Mapa valle')
