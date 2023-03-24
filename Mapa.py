34a12e3012426d5a59ffd013a5dedd3122b05122
Mariana Hernandez <111605652+Marihp@users.noreply.github.com>
2023-03-23 21:19:17 -0500

    Agregar comentarios

--- Mapa.py
+++ Mapa.py
@@ -1,26 +1,31 @@
+# Importar las librerias folium, pandas, geopandas y numpy
 import folium
 import pandas as pd
 import geopandas as gpd
 import numpy as np
 
-
+# Leer la base de datos
 municipios = gpd.read_file("/Users/marianahernandez/biciMaps/MunicipiosVeredas.json")
+# Realizar una copia para agregar index
 municipios_index = municipios.copy()
 municipios_index = municipios_index.set_index("MPIO_CNMBR")
 # Eliminar columnas innecesarias
 municipios = municipios.drop(columns = ["MPIO_CCNCT", "MPIO_CCDGO"])
 # Municipios de antioquia
 municipios_ant = municipios[municipios["DPTO_CCDGO"] == "05"]
+# Sacar los municipios del Valle de Aburra
 municipios_valle = municipios_index[municipios_index["DPTO_CCDGO"] == "05"]
 municipios_valle = municipios_valle.loc[["MEDELLÍN", "ENVIGADO", "SABANETA","ITAGÜÍ", "CALDAS", "LA ESTRELLA", "COPACABANA", "BELLO", "BARBOSA", "GIRARDOTA"]]
+# Resetear los indices para evitar problemas con los poligonos
 municipios_valle = municipios_valle.reset_index()
 print(municipios_valle.head())
 
 # Crear mapa locacion, estilo, zoom
 m = folium.Map(location=[6.2 , -75.5], tiles='cartodbpositron', zoom_start=10)
 
+# Ciclo for para mapear cada uno de los municipios del valle
 for _, r in municipios_valle.iterrows():
-    # Simplificar la tolerancia para que el mapa se muestre
+    # Simplificar la tolerancia para que el mapa se muestre sin errores
     sim_geo = gpd.GeoSeries(r['geometry']).simplify(tolerance=0.001)
     geo_j = sim_geo.to_json()
     # Dar formato al poligono
@@ -30,4 +35,6 @@ for _, r in municipios_valle.iterrows():
     folium.Popup(r['MPIO_CNMBR']).add_to(geo_j) 
     # Agregar poligono al mapa
     geo_j.add_to(m)
+
+# Finalmente guardar nuestro mapa
 m.save('mapaValle')
