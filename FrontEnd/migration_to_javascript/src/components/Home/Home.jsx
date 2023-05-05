8aaf976b10206e59a462cfe60eba7214c5c3789f
marianahernandez <marhernandezpa@unal.edu.co>
2023-05-05 10:24:30 -0500

    Type effect working+

--- FrontEnd/migration_to_javascript/src/components/Home/Home.jsx
+++ FrontEnd/migration_to_javascript/src/components/Home/Home.jsx
@@ -1,21 +1,35 @@
 import "../../App.css";
-import Typed from "react-typed";
+import Typed from "typed.js";
 import bike from "../../assets/bikerdribbble.gif";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 function Home() {
+  const el = useRef(null);
+
   useEffect(() => {
-    const typed = new Typed(".multiText", {
-      strings: ["una empresa", "un equipo"],
+    const typed = new Typed(el.current, {
+      strings: ["Pasión", "Rapidez", "Soluciones", "familia", "BiciMaps"], // Strings to display
+      // Speed settings, try diffrent values untill you get good results
+      startDelay: 300,
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 100,
     });
+
+    // Destropying
+    return () => {
+      typed.destroy();
+    };
   }, []);
+
   return (
     <>
       <div className="home">
         <div className="cols0">
           <span className="topline">Hola</span>
+          <h1>Somos</h1>
           <h1>
-            Somos <span className="multiText"></span>
+            <span className="multiText" ref={el}></span>
           </h1>
           <p> Aún estamos en proceso de construcción.</p>
         </div>
