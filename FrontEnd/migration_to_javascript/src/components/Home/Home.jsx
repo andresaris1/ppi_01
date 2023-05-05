a151bf762eeb8e90857206ce0490f278542ebdaf
marianahernandez <marhernandezpa@unal.edu.co>
2023-05-05 09:53:49 -0500

    Try home with type effect

--- FrontEnd/migration_to_javascript/src/components/Home/Home.jsx
+++ FrontEnd/migration_to_javascript/src/components/Home/Home.jsx
@@ -1,18 +1,32 @@
-import '../../App.css'
+import "../../App.css";
+import Typed from "react-typed";
+import bike from "../../assets/bikerdribbble.gif";
+import { useEffect } from "react";
 
-
-function Home(){
-    return(
-        <>
-        <div className="content wrapper">
-            <div className="textBox">
-                <h2>¿Rodar por el valle? <br/> <span> BiciMaps</span></h2>
-                <p> Aún estamos en proceso de construcción.
-                </p>
-            </div>
+function Home() {
+  useEffect(() => {
+    const typed = new Typed(".multiText", {
+      strings: ["una empresa", "un equipo"],
+    });
+  }, []);
+  return (
+    <>
+      <div className="home">
+        <div className="cols0">
+          <span className="topline">Hola</span>
+          <h1>
+            Somos <span className="multiText"></span>
+          </h1>
+          <p> Aún estamos en proceso de construcción.</p>
         </div>
-        </>
-    )
+      </div>
+      <div className="cols1">
+        <div className="bike">
+          <img src={bike} alt="Joven pedaleando" />
+        </div>
+      </div>
+    </>
+  );
 }
 
-export { Home }
+export { Home };
