49a5c82a97a52b165758a95a2d327cb425a21004
marianahernandez <marhernandezpa@unal.edu.co>
2023-05-12 09:30:32 -0500

    Home funcional

--- FrontEnd/migration_to_javascript/src/components/Home/Home.jsx
+++ FrontEnd/migration_to_javascript/src/components/Home/Home.jsx
@@ -2,8 +2,10 @@ import { useEffect, useRef } from "react";
 import React from "react";
 // Import library typed effect
 import Typed from "typed.js";
+// Import icons
+import { FaGithub } from "react-icons/fa";
 // Import gif biker
-import bike from "../../assets/bikerdribbble.gif";
+import bike from "../../assets/Bici-landing.svg";
 
 // Home component
 const Home = () => {
@@ -28,29 +30,61 @@ const Home = () => {
       typed.destroy();
     };
   }, []);
+
+  // Redirect to github
+  const githubRedirect = () => {
+    window.location.href = "https://github.com/andresaris1/ppi_01";
+  };
+
+  const buyMeACoffee = () => {
+    window.location.href = "https://www.buymeacoffee.com/bicimaps";
+  };
+
   return (
-    // Crear sección con grid de 8 columnas a lo ancho de la pantalla
+    // Home section
     <section className="h-[90vh] w-screen grid grid-cols-1 md:grid-cols-8">
+      {/* First col content */}
       <div className="md:col-span-5 flex flex-col items-left justify-center gap-8 p-20">
-        {/* Titulo del home */}
+        {/* Text title */}
         <div className="gap-4">
           <span className="text-6xl font-bold">Hola,</span>
           <h1 className="text-7xl font-bold text-[#1E293B]">
-            Somos <span className="text-[#38BDF8]" ref={typ}></span>
+            Somos <span className="text-bl" ref={typ}></span>
           </h1>
+          {/* Second part of first col */}
         </div>
+        {/* Description paragraph */}
         <p className="text-gray-600 text-2xl ">
           Somos una comunidad de ciclistas que busca mejorar la experiencia de
           navegación de los ciclistas en el Valle de Aburrá, y sí tenemos
           integración con EnCicla, por si dejaste la bici en casa.
         </p>
-        <div>
-          <button className="bg-[#38BDF8] text-white font-bold text-xl px-4 py-2 rounded-md">
-            Apoyarnos con un café
+
+        {/* Buttons */}
+        <div className="flex item-center gap-8">
+          {/* Button buy me a coffee */}
+          <button
+            onClick={buyMeACoffee}
+            class="bg-transparent hover:bg-bl text-gray-700 font-semibold hover:text-white py-2 px-4 border border-bluel-500 hover:border-transparent rounded"
+          >
+            Apóyanos con un café
+          </button>
+
+          {/* Button repo in GitHub */}
+          <button
+            onClick={githubRedirect}
+            class="bg-transparent flex items-center gap-2 text-left leading-4 "
+          >
+            <FaGithub size="2em" />
+            Ver <br /> el código
           </button>
         </div>
       </div>
-      <div className="bg-red-500 md:col-span-3">Image</div>
+
+      {/* Contenido segunda columna */}
+      <div className="md:col-span-3">
+        <img src={bike} />
+      </div>
     </section>
   );
 };
--- FrontEnd/migration_to_javascript/tailwind.config.js
+++ FrontEnd/migration_to_javascript/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bl: "#38BDF8",
+        bh: "#2D9ECF",
+      },
+    },
   },
   plugins: [],
 };
